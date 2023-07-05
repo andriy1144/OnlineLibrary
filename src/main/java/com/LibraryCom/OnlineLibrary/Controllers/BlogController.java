3120dbc5511b7cc9649ac13bcf96a24d76dbd3ef
Andriy Murhan <andrijmurgan@gmail.com>
2023-07-05 20:32:06 +0300

    Created Blog page(Front-end)

---
+++ src/main/java/com/LibraryCom/OnlineLibrary/Controllers/BlogController.java
@@ -0,0 +1,29 @@
+package com.LibraryCom.OnlineLibrary.Controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/blog")
+public class BlogController {
+
+    //Required variables
+    private static final Logger logger = LoggerFactory.getLogger(BlogController.class);
+
+    //Controllers
+    @GetMapping("/")
+    public String blogPage(Model model){
+
+        attributes :{
+            model.addAttribute("isAdmin",true);
+        }
+
+        return "blogPage";
+    }
+
+
+}
