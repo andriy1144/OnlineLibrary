9db9f580b70190740cc9e88f1e97df0d71842171
Andriy Murhan <andrijmurgan@gmail.com>
2023-07-09 16:28:53 +0300

    Change addBlogPage (frontEnd)

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,10 @@
+const fileInputer = document.getElementById("fileInputer");
+let counter = 2;
+
+function addMoreInputer() {
+    fileInputer.innerHTML += " <div class=\"mb-3\">" +
+        "                           <label for=\"formFileMultiple\" class=\"form-label\">File " + counter + "</label>" +
+        "                           <input class=\"form-control\" type=\"file\" name=\"files\" id=\"formFileMultiple\" multiple>" +
+        "                       </div>"
+    counter++;
+}
